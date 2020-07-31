03a9fe38dc1b9d29e0971157e1bf9f2c6550796f
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-07-31 14:17:51 +0700

    create table user

---
+++ src/main/java/com/codegym/demodeploy/model/User.java
@@ -0,0 +1,15 @@
+package com.codegym.demodeploy.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+}
